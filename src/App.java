e45ef4d8907fe2d6f6ce7386181d02b9484539da
Sakthivel Ramamoorthi <sakthivel.ramaoorthi2001@gmail.com>
2023-11-11 18:14:14 +0530

    mergeStringAlternatively 1768

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //megre String Alternativly
+        MergeStringAlternatively mergeStringAlternatively = new MergeStringAlternatively();
+        String mergeStringAlternativelyResult =  mergeStringAlternatively.MergreTwoString("abcd", "efgh");
+        System.out.println(mergeStringAlternativelyResult);  //output   aebfcgdh
+    }
+}
---
+++ src/MergeStringAlternatively.java
@@ -0,0 +1,25 @@
+class MergeStringAlternatively {
+
+
+    public String MergreTwoString(String one, String two){
+        int i = 0;
+        String tempStr = "";
+
+        while(i != (one.length()  + two.length())) {
+            if(i <= one.length() - 1){
+                tempStr += one.charAt(i);
+            }
+            if(i <= two.length() - 1) {
+                  tempStr += two.charAt(i);
+            }
+            i++;
+        }
+
+        return tempStr;
+    }
+
+
+    public String mergeAlternately(String word1, String word2) {
+        return MergreTwoString(word1, word2);
+    }
+}
